07d8d244fcca127589bdbe2de65a1d131c96b44c
DavidDimasPatty <daviddimas80@gmail.com>
2022-03-20 13:38:23 +0000

    routing problem 5

--- server.js
+++ server.js
@@ -6,18 +6,9 @@ const router = jsonServer.router("db.json");
 const PORT = process.env.PORT || 3000;
 
 app.use("/", express.static("build"), router);
-
+app.use("/customers", express.static("build"), router);
 if (process.env.NODE_ENV === "production") {
-  app.get("/", function (req, res) {
-    res.sendFile(path.resolve(__dirname + "./build/index.html"));
-  });
-  app.get("/home", function (req, res) {
-    res.sendFile(path.resolve(__dirname + "./build/index.html"));
-  });
-  app.get("/customers", function (req, res) {
-    res.sendFile(path.resolve(__dirname + "./build/index.html"));
-  });
-  app.get("/customers/:id", function (req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.resolve(__dirname + "./build/index.html"));
   });
 }
