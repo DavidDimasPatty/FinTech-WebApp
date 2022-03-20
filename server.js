06274d2a4b95fa871c65de5ef6914e2cd2ab2d6c
DavidDimasPatty <daviddimas80@gmail.com>
2022-03-20 05:51:48 +0000

    router fix 2

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 const jsonServer = require("json-server");
 const server= jsonServer.create();
-const path = require('path');
-const router = jsonServer.router('db.json');
-const express = require('express');
+const router=jsonServer.router("./db.json");
 const cors = require("cors");
-const middlewares = jsonServer.defaults();
+const middleware=jsonServer.defaults({
+  static:"./build"
+})
 const port=process.env.PORT||5000;
+server.use(middleware);
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
   }
   )
 );
-server.use('/db', middlewares, router);
-server.use(express.static(path.join(__dirname, 'build')));
 
-server.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+server.use(router);
 server.use(cors());
 server.listen(port,()=>{
   console.log(`Server is running on ${port}`);
 })
-
-//const jsonServer = require('json-server');
-//const app = jsonServer.create();
-
