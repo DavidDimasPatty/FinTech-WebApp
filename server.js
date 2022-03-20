1d0b10263e48b80638c93eb30ddfb582a51779f6
DavidDimasPatty <daviddimas80@gmail.com>
2022-03-20 14:19:02 +0000

    routing problem 8

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const jsonServer = require("json-server");
-const path = require("path");
-const express = require("express");
-const app = express();
-const router = jsonServer.router("db.json");
-const PORT = process.env.PORT || 3000;
-
-if (process.env.NODE_ENV === "production") {
-
-  app.use("/api", express.static("build"), router);
-}
-else{
-  app.use("/", express.static("build"), router);
-}
+const server=jsonServer.create();
+const router = jsonServer.router("./db.json");
+const PORT = process.env.PORT || 5000;
+const middleware =jsonServer.defaults({
+  '/api/':'/$1'
+})
+server.use(middleware);
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
