19005af757f1961e361f2c978e5a710afacd93e6
DavidDimasPatty <daviddimas80@gmail.com>
2022-03-20 14:20:19 +0000

    routing problem 9

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server=jsonServer.create();
 const router = jsonServer.router("./db.json");
 const PORT = process.env.PORT || 5000;
 const middleware =jsonServer.defaults({
-  '/api/':'/$1'
+  '/api/*':'/$1'
 })
 server.use(middleware);
 
