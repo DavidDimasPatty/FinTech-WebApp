02780785b480c614731d19a73daecc7f6864b1e7
DavidDimasPatty <daviddimas80@gmail.com>
2022-03-20 12:52:53 +0000

    routing problem

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const jsonServer = require("json-server");
 const server= jsonServer.create();
 const router=jsonServer.router("./db.json");
 const cors = require("cors");
-const middleware=jsonServer.defaults();
+const middleware=jsonServer.defaults({
+  static:"./build"
+})
 const port=process.env.PORT||5000;
 server.use(middleware);
 server.use(
