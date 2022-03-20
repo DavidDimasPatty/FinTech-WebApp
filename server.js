da938af1363b8327d4621ead0a95e8390fd32579
DavidDimasPatty <daviddimas80@gmail.com>
2022-03-20 14:48:18 +0000

    routing problem 10

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+const cors= require('cors')
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
   static: './build'
@@ -13,6 +14,7 @@ server.use(jsonServer.rewriter({
   '/api/customers/:id' : '/customers/:id'
 }))
 server.use(router);
+server.use(cors());
 server.listen(PORT, () => {
   console.log('Server is running');
 });
