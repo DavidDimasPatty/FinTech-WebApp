27c57c44235bc4b44f51b747e8b7a07854253f35
DavidDimasPatty <daviddimas80@gmail.com>
2022-03-20 14:06:53 +0000

    routing problem 7

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const app = express();
 const router = jsonServer.router("db.json");
 const PORT = process.env.PORT || 3000;
 
-app.use("/*", express.static("build"), router);
 if (process.env.NODE_ENV === "production") {
-  app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname + "./build/index.html"));
-  });
+
+  app.use("/api", express.static("build"), router);
+}
+else{
+  app.use("/", express.static("build"), router);
 }
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
