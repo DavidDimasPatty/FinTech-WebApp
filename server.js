4f071045fe682c7541754cb3a7d6956cbf940994
DavidDimasPatty <daviddimas80@gmail.com>
2022-03-21 00:42:21 +0000

    routing problem 11

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use('/api', jsonServer.router('db.json'),jsonServer.defaults({noCors: false}))
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 app.listen(PORT, () => {
   console.log(`Server is running ${PORT}`);
 });
