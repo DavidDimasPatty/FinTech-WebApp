4cb22c113d5d72e9bc176340a2ce5cb5384da970
DavidDimasPatty <daviddimas80@gmail.com>
2022-03-21 00:23:43 +0000

    routing problem 11

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
 const express= require("express")
 const cors= require('cors')
 const path = require('path')
 const app = express();
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults({
-  static: './build'
-});
+app.use(cors())
+
 const PORT = process.env.PORT || 5000;
-server.use(middlewares);
-server.use(cors());
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1'
-}))
-server.use(router);
-server.listen(PORT, () => {
-  console.log('Server is running');
+
+
+app.use('/api', jsonServer.router('db.json'),jsonServer.defaults({noCors: false}))
+app.listen(PORT, () => {
+  console.log(`Server is running ${PORT}`);
 });
 
